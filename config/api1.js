db288c1b452c5482b40d1d40bf5b57c834687596
CodeDeyya <codedeyya@gmail.com>
2021-06-11 17:45:35 +0530

    setup

--- config/api1.js
+++ config/api1.js
@@ -1,4 +1,4 @@
   
 export default {
-  API_BASE_URL: 'http://3.1.94.73:8082'
+  API_BASE_URL: 'http://localhost:8082'
 }
--- pages/landing.js
+++ pages/landing.js
@@ -108,7 +108,7 @@ export default function LandingPage(props) {
         }}
         {...rest}
       />
-      <Parallax filter responsive image={require("assets/img/landing.jpg")}>
+      <Parallax filter responsive image={require("assets/img/LANDING.png")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
